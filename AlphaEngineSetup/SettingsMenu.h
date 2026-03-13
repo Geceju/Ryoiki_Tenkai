7d729fef7d650f92968e39c3475c40ed7375a06f
succ-duc <dylanlimjt@gmail.com>
2026-03-14 04:13:56 +0800

    Added settings menu (no functionality)
    Volume buttons (Music and SFX): actual functionality to be added
    
    VSync button: uses AEGfxSetVSync
    
    - game is paused when in settings menu (includes animation in main menu)

---
+++ AlphaEngineSetup/SettingsMenu.h
@@ -0,0 +1,18 @@
+#ifndef SETTINGS_MENU_H
+#define SETTINGS_MENU_H
+
+#include "AEEngine.h"
+
+// Global settings shared across the entire game
+extern float g_MusicVolume;
+extern float g_SFXVolume;
+extern bool g_VSyncEnabled;
+
+// Core functions to manage the settings overlay
+void SettingsMenu_Load();
+void SettingsMenu_Initialize();
+void SettingsMenu_Update(bool& isMenuOpen);
+void SettingsMenu_Draw(bool isIngame); // Pass true if in a level to draw the "Exit to Menu" button
+void SettingsMenu_Unload();
+
+#endif
