d867b9a0cc1d60ff846fd6f042bad0ca9c3e5bf7
Winson <winsonteo48@gmail.com>
2026-04-05 20:37:19 +0800

    Update Credits.cpp

--- AlphaEngineSetup/Credits.cpp
+++ AlphaEngineSetup/Credits.cpp
@@ -50,9 +50,9 @@ static const std::vector<std::string> credits = {
     "WWW.DIGIPEN.EDU",
     "All content \xC2\xA9 2026 DigiPen Institute of Technology Singapore. All Rights Reserved",
     "",
-    "--- MUSIC COPYRIGHTS ---",
+//    "--- MUSIC COPYRIGHTS ---",
 //add music copyright here
-    "",
+//    "",
     "--- LECTURERS ---",
     "Prof Tommy",
     "Prof Soroor",
@@ -67,7 +67,6 @@ void Credits_Load() {
     if(pSquareMesh == nullptr) pSquareMesh = CreateSquare();
     pLogo = AEGfxTextureLoad("Assets/DigiPen_BLACK.png");
     fontId = AEGfxCreateFont("Assets/exo2-regular.ttf", 24);
-    //bgm = AEAudioLoadMusic("Assets/Sounds/credits_bgm.mp3");
 }
 
 void Credits_Init() {
@@ -77,9 +76,6 @@ void Credits_Init() {
     y_pos = -1.2f;
     logo_y = 0.0f; // AE center is (0,0)
     num_lines = (int)credits.size();
-
-    // Play BGM
-   // AEAudioPlay(bgm, bgmGroup, 1.0f, 1.0f, -1); // -1 for infinite loop
 }
 
 void Credits_Update() {
@@ -100,7 +96,6 @@ void Credits_Update() {
     else {
         y_pos += 0.3f * dt;
         float line_spacing = 0.1f;
-        // Check if finished
         if (y_pos - (num_lines * line_spacing) > 1.0f ) {
             gGameStateNext = GS_MAINMENU;
         }
@@ -108,31 +103,26 @@ void Credits_Update() {
 }
 
 void Credits_Draw() {
-    // Clear screen first
+
     AEGfxSetBackgroundColor(0.15f, 0.15f, 0.15f);
-    AEGfxSetRenderMode(AE_GFX_RM_COLOR); // Start with a neutral mode
+    AEGfxSetRenderMode(AE_GFX_RM_COLOR);
     AEGfxSetBlendMode(AE_GFX_BM_BLEND);
 
     if (!logo_done) {
 
-        AEGfxSetBackgroundColor(0.5f, 0.5f, 0.5f); // Grey so white logo is visible
+        AEGfxSetBackgroundColor(0.5f, 0.5f, 0.5f); 
         AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
 
-        // 2. THE TINT RESET (CRITICAL)
-        // Multiply by 1.0 means "keep original color"
         AEGfxSetColorToMultiply(1.0f, 1.0f, 1.0f, 1.0f);
-        // Add 0.0 means "don't brighten/wash out"
         AEGfxSetColorToAdd(0.0f, 0.0f, 0.0f, 0.0f);
         AEGfxSetTransparency(1.0f);
 
-        // 3. Transformation
         AEMtx33 scale, trans, total;
         AEMtx33Scale(&scale, AEGfxGetWindowWidth()-400.0f, AEGfxGetWindowHeight()-400.0f);
         AEMtx33Trans(&trans, 0.0f, 0.0f);
         AEMtx33Concat(&total, &trans, &scale);
         AEGfxSetTransform(total.m);
 
-        // 4. Bind and Draw
         if (pLogo != nullptr) {
 
             AEGfxTextureSet(pLogo, 0, 0);
@@ -148,7 +138,6 @@ void Credits_Draw() {
         AEGfxPrint(fontId, copyright, -0.4f, -0.6f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
     }
     else {
-        // Reset transform for credits just in case
         AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);
         AEMtx33 total;
         AEMtx33Identity(&total);
